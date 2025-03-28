b984dc25cdeda1c3b240fd61e91f127fa62c97bf
TuanVux <tuanvukg2703@gmail.com>
2025-03-28 08:53:21 +0700

    sidebar

--- src/layout/nainLayout.jsx
+++ src/layout/nainLayout.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Nav from '../pages/nav/Nav';
+import SideBar from '../pages/side/SideBar';
 
 const MainLayout = () => {
   return (
-    <div>
-        <Nav/>
-      <main className='main '> 
-        <Outlet />
-      </main>
+    // <div>
+    //   <Nav />
+    //   <main className='main '>
+    //     <Outlet />
+    //   </main>
+    // </div>
+    <div className='bg-[#030303] text-white'>
+      <Nav />
+      <div className="grid grid-cols-6 grid-rows-5 gap-2 h-screen ">
+        <div className=" row-span-5 "><SideBar /></div>
+        <main className=" col-span-5 row-span-5">
+          <Outlet />
+        </main>
+      </div>
     </div>
+
   );
 };
 
