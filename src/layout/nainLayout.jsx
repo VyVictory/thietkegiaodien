c15acf4f22a0c0530ff951fb3c166d1f987c3c81
VyVictory <145247364+VyVictory@users.noreply.github.com>
2025-03-28 10:16:30 +0700

    new layout

--- src/layout/nainLayout.jsx
+++ src/layout/nainLayout.jsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Nav from '../pages/nav/Nav';
-import SideBar from '../pages/side/SideBar';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Nav from "../pages/nav/Nav";
+import SideBar from "../pages/side/SideBar";
 
 const MainLayout = () => {
+  const hightNavBar = "60px";
   return (
-    // <div>
-    //   <Nav />
-    //   <main className='main '>
-    //     <Outlet />
-    //   </main>
-    // </div>
-    <div className='bg-[#030303] text-white body'>
-      <Nav />
-      <div className="grid grid-cols-6 grid-rows-5 gap-2 h-screen main">
-        <div className=" row-span-5 "><SideBar /></div>
-        <main className=" col-span-5 row-span-5">
-          <Outlet />
-        </main>
+    <>
+      <div className="h-screen flex flex-col bg-white text-white">
+        <nav
+          className={`h-[${hightNavBar}] fixed top-0 bg-black left-0 w-full flex items-center justify-center  text-white shadow-md z-50`}
+        >
+          <Nav />
+        </nav>
+        <div className={`flex mt-[${hightNavBar}]`}>
+          <aside
+            className={`w-1/6 bg-gray-200 flex flex-col items-center sticky top-[${hightNavBar}] h-[calc(100vh-${hightNavBar})]`}
+          >
+            <div className="p-2 w-full h-full bg-black">
+              <SideBar />
+            </div>
+          </aside>
+          <main className="w-5/6 bg-gray-400 p-4">
+            <Outlet />
+            <div className="h-[3000px]"></div>
+          </main>
+        </div>
       </div>
-    </div>
-
+    </>
   );
 };
 
--- src/pages/nav/Nav.jsx
+++ src/pages/nav/Nav.jsx
@@ -1,14 +1,10 @@
 import * as React from "react";
 import NavCss from "../../css/NavCss.css";
 import { PlayArrowSharp } from "@mui/icons-material";
-export default function Nav() {
-  scrollCss();
+export default function Nav() { 
   return (
     <>
-      <nav className={`Nav`}>
-        {" "}
-        <PlayArrowSharp className="w-2 h-2" />
-      </nav>
+      <nav className={`bg-black w-full h-full border-b border-gray-400`}>NAV</nav>{" "}
     </>
   );
 }
@@ -16,11 +12,9 @@ const scrollCss = () => {
   window.addEventListener("scroll", function () {
     let navbar = document.querySelector(".Nav");
     if (window.scrollY > 10) {
-      navbar.style.backgroundColor = "rgba(0, 0, 0, 1)"; // Màu nền trắng mờ
-      navbar.style.backdropFilter = "blur(10px)"; // Hiệu ứng làm mờ nền phía sau
-    } else {
       navbar.style.backgroundColor = "rgba(0, 0, 0, 0.75)"; // Màu nền trắng mờ
-      navbar.style.backdropFilter = "blur(10px)"; // Hiệu ứng làm mờ nền phía sau
+    } else {
+      navbar.style.backgroundColor = "black"; // Màu nền trắng mờ
     }
   });
 };
