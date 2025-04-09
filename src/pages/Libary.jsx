2b2fcadb8a75f99ac769325eaa1a7ed89af3ecea
TuanVux <tuanvukg2703@gmail.com>
2025-04-09 14:38:11 +0700

    libary

--- src/pages/Libary.jsx
+++ src/pages/Libary.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import MyList from '../components/music/MyList'
 
 export default function Libary() {
   return (
-    <div>Libary</div>
+    <div>
+      <div className='flex gap-1'>
+        <button className='border-b-2'>
+          <span>THƯ VIỆN</span>
+        </button>
+        <button>
+          <span>NỘI DUNG TẢI XUỐNG</span>
+        </button>
+      </div>
+      <div className='font-bold text-2xl my-5'>Nhạc của tôi</div>
+      <MyList />
+    </div>
   )
 }
