98cf6192781838f18d28c15baf181f52a1ad916c
VyVictory <145247364+VyVictory@users.noreply.github.com>
2025-05-01 06:50:06 +0700

    Update Nav.jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -25,9 +25,11 @@ export default function Nav() {
       ref={navRef}
       className="fixed top-0 left-0 w-full h-[60px] flex items-center justify-between pr-4 border-b border-transparent z-50 transition-colors duration-300 bg-transparent"
     >
-      <div className="flex-shrink-0 flex flex-row items-center gap-2 text-blue-400 font-bold text-xl">
-        <img src={log} alt="Logo" className="h-16 object-contain" />
-        Next To Music
+      <div className="flex-shrink-0 text-blue-400 font-bold text-xl">
+        <div className="  flex-row items-center hidden xl:flex">
+          <img src={log} alt="Logo" className="h-16 object-contain" />
+          Next To Music
+        </div>
       </div>
       <div className="flex-1 px-4 hidden md:flex justify-center">
         <div className="relative w-full max-w-md ">
